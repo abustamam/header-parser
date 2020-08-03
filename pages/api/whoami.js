f786a5e5f93e5826ca3382f3b4ac9abfa6c15b0d
Rasheed Bustamam <rasheed@fullstacklabs.co>
2020-08-03 15:57:43 -0700

    initial commit

---
+++ pages/api/whoami.js
@@ -0,0 +1,19 @@
+import Cors from 'cors';
+
+import initMiddleware from '../../lib/init-middleware';
+
+const cors = initMiddleware(
+  Cors({
+    methods: ['GET'],
+  })
+);
+
+export default async (req, res) => {
+  await cors(req, res);
+  const { headers, connection } = req;
+  const ip = headers['x-real-ip'] || connection.remoteAddress;
+  const language = headers['accept-language'];
+  const software = headers['user-agent'];
+  res.statusCode = 200;
+  res.json({ ip, language, software });
+};
